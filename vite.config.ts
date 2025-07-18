7cf2f7b70435f5215fe9d5c5f1e79ddf8e01c998
tkwiatkowskii <tomaszkwiatkowski191@gmail.com>
2025-07-18 17:34:26 +0200

    scaffolding

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      base: './',
+      server: {
+        open: true,
+        port: 7001,
+        watch: {
+          usePolling: true,
+          interval: 1000,
+        }
+      },
+    };
+  } else {
+    return {
+      base: './',
+      build: {
+        outDir: 'dist',
+        sourcemap: true,
+        minify: true,
+        emptyOutDir: true,
+      },
+    };
+  }
+});
