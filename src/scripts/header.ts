ec9e5da33d0d5e2234f53192b90ce34f538f6129
tkwiatkowskii <tomaszkwiatkowski191@gmail.com>
2025-07-23 02:07:34 +0200

    Added main content styling + prismJS

--- src/scripts/header.ts
+++ src/scripts/header.ts
@@ -2,12 +2,16 @@ export default class Header {
   private button: HTMLButtonElement | null;
   private headerNav: HTMLDivElement | null;
   private headerWrapper: HTMLDivElement | null;
+  private themeButton: HTMLButtonElement | null;
+  private siteBody: HTMLBodyElement | null;
   private windowMedia: MediaQueryList;
 
   constructor() {
+    this.themeButton = document.querySelector<HTMLButtonElement>('.navigation__theme-icon');
     this.button = document.querySelector<HTMLButtonElement>('.header__button');
     this.headerNav = document.querySelector<HTMLDivElement>('.header__navigation-menu');
     this.headerWrapper = document.querySelector<HTMLDivElement>('.layout__header');
+    this.siteBody = document.querySelector('body');
     this.windowMedia = window.matchMedia('(min-width: 768px)');
 
     this.displayHeader();
@@ -17,6 +21,7 @@ export default class Header {
     this.updateImage();
     this.expandNavigation();
     this.configureHighlightSize();
+    this.changeThemeMode();
   }
 
   private updateImage(): void {
@@ -27,17 +32,17 @@ export default class Header {
     const img = this.button.querySelector('img');
     if (!img) throw new Error("Couldn't update image");
 
-    const updateImg = (): void => {
+    const updateImg = () : void => {
       if (this.windowMedia.matches) {
         img.src = './brightness.svg';
-        img.alt = 'Brightness icon';
-        img.classList.add('header__icon--brightness');
+        img.alt = 'Theme icon';
+        img.classList.add('header__icon--theme');
         img.classList.remove('header__icon--menu');
       } else {
         img.src = './menu-icon.svg';
         img.alt = 'Menu icon';
         img.classList.add('header__icon--menu');
-        img.classList.remove('header__icon--brightness');
+        img.classList.remove('header__icon--theme');
       }
     };
 
@@ -52,24 +57,21 @@ export default class Header {
     this.button.addEventListener('pointerup', () => {
       const img = this.button!.firstElementChild;
       if (img?.classList.contains('header__icon--menu')) {
-        this.headerNav!.classList.toggle('header__menu--collapsed');
+        this.headerNav!.classList.toggle('header__menu--collapse');
         this.button!.classList.toggle('header--activated-mobile');
         this.headerWrapper!.classList.toggle('header--activated-mobile');
       }
       else {
-        const layout = document.querySelector('body')
-        if (!layout) throw new Error("Couldn't add light mode setting");
-
-        const currentLightModeSetting : string = getComputedStyle(layout)
-          .getPropertyValue('--mode');
-
-        if (currentLightModeSetting === 'light') {
-          layout.style.setProperty('--mode', 'dark')
-        }
-        else {
-          layout.style.setProperty('--mode', 'light')
-        }
+        if (!this.siteBody) throw new Error("Couldn't add light mode setting");
+        this.toggleTheme();
       }
+    });
+
+    this.windowMedia.addEventListener('change', () => {
+      if (this.windowMedia.matches) {
+        this.headerNav?.classList.remove('header__menu--collapse');
+        this.headerWrapper?.classList.remove("header--activated-mobile");
+      };
     });
   }
 
@@ -97,6 +99,28 @@ export default class Header {
 
     calculateOffset();
     window.addEventListener('resize', calculateOffset);
+  }
+
+  private changeThemeMode() : void {
+    if (!this.themeButton) throw new Error("Couldn't add theme event handler");
+
+    this.themeButton.addEventListener('pointerup', () => {
+      this.toggleTheme();
+    })
+  }
+
+  private toggleTheme() : void {
+    if (!this.siteBody) throw new Error("Couldn't add theme event handler");
+
+    const currentThemeSetting : string = getComputedStyle(this.siteBody)
+      .getPropertyValue('--mode');
+
+    if (currentThemeSetting === 'light') {
+      this.siteBody.style.setProperty('--mode', 'dark')
+    }
+    else {
+      this.siteBody.style.setProperty('--mode', 'light')
+    }
   }
 
   public static async init() : Promise<void> {
