ffe1233f83cfd0e125b3b0c5a051668a2320e5bf
tkwiatkowskii <tomaszkwiatkowski191@gmail.com>
2025-07-26 15:54:12 +0200

    Added native browser language check. Finished styling

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,11 +3,13 @@ import '/src/styles/main.css';
 import Sidebar from "./sidebar";
 import Header from "./header";
 import { typewriterEffect } from "./typewriter.js";
+import setThemeOnPreference from './theme.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   (async () => {
     await Promise.all([
       typewriterEffect(),
+      setThemeOnPreference(),
       Header.init(),
       Sidebar.init()
     ]);
