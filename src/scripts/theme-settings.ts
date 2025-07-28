3b6ec22dddfdfc90aa858d2e042341679a21e87b
tkwiatkowskii <tomaszkwiatkowski191@gmail.com>
2025-07-29 00:09:50 +0200

    Added dynamic uml diagram theme options

--- src/scripts/theme-settings.ts
+++ src/scripts/theme-settings.ts
@@ -1,10 +1,12 @@
 export default class ThemeConfig {
   private layout: HTMLDivElement | null;
   private themeButton: HTMLButtonElement | null;
+  private umlDiagram: HTMLPictureElement | null;
 
   constructor() {
     this.layout = document.querySelector<HTMLDivElement>('.layout');
     this.themeButton = document.querySelector<HTMLButtonElement>('.navigation__theme-icon');
+    this.umlDiagram = document.querySelector<HTMLPictureElement>('.section__uml-picture');
   }
 
   private changeThemeMode() : void {
@@ -12,6 +14,7 @@ export default class ThemeConfig {
 
     this.themeButton.addEventListener('pointerup', () => {
       this.toggleTheme();
+      this.changeUmlDiagramTheme();
     })
   }
 
@@ -36,8 +39,35 @@ export default class ThemeConfig {
     }
   }
 
+  public changeUmlDiagramTheme() : void {
+    this.umlDiagram = document.querySelector<HTMLPictureElement>('.section__uml-picture');
+
+    if(!this.layout || !this.umlDiagram) throw new Error("Couldn't set up uml load");
+
+    const currentTheme : string | null = this.layout.getAttribute('data-theme');
+
+    if (localStorage.getItem('theme') !== null) {
+      this.umlDiagram.innerHTML = `
+          <img class="section__uml-picture" src="/uml/uml-inheritance-${currentTheme}.svg" 
+          alt="UML inheritance diagram" />
+      `;
+    };
+  }
+
+  public setThemeOnPreference() {
+    if (!this.layout) throw new Error("Couldn't configure theme");
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark').matches) {
+      this.layout.setAttribute('data-theme', 'dark');
+    } else {
+      this.layout.setAttribute('data-theme', 'light');
+    };
+  }
+
   public init() : void {
+    this.setThemeOnPreference();
     this.changeThemeMode();
     this.loadSavedTheme();
+    this.changeUmlDiagramTheme();
   }
 }
